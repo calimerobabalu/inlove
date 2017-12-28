58c58f027f2d6198d2082c011153d727ba97506c
Giacomo Bresciani <giacomo.bresciani@facile.it>
2017-12-28 01:49:10 +0100

    Final commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     sectionsColor: ['yellow', '#3D464D', '#C0C0C0', '#ADD8E6'],
+    scrollingSpeed: 700,
+    verticalCentered:false,
     afterLoad: function(anchorLink, index) {
       var loadedSection = $(this);
 
       //using anchorLink
-      if (index == 5) {
+      if (index == 8) {
         startShake()
         window.setTimeout(stopShake, 2000);
       }
@@ -13,7 +15,7 @@ $(document).ready(function() {
     onLeave: function(index, nextIndex, direction) {
       var leavingSection = $(this);
 
-      if (index == 5) {
+      if (index == 8) {
         stopShake()
       }
     }
